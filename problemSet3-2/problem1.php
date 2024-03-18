f73c772d5ed10c3e0988631d1dff094d66bc3d22
Shuo Yang <shuoy.ny@gmail.com>
2024-03-18 15:57:20 -0400

    implement problem1

--- problemSet3-2/problem1.php
+++ problemSet3-2/problem1.php
@@ -1,6 +1,14 @@
 <?php
 
-echo "hello world";
+$n = $_GET['n'];
+
+
+echo "<h2>Times table for $n</h2>";
+
+for( $i = 1; $i <= 12; $i++){
+    $result = $i * $n;
+    echo "$i x $n = $result <br>";
+}
 
 
 ?>
@@ -11,9 +19,9 @@ echo "hello world";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>problem1</title>
 </head>
 <body>
-    
+
 </body>
 </html>
