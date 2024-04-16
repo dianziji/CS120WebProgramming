87320ee4dbfcf71a80ffe78126b36ad788dbfc38
dianziji <47435582+dianziji@users.noreply.github.com>
2024-04-16 18:36:52 -0400

    Update hello_app.js

--- hello_app.js
+++ hello_app.js
@@ -9,6 +9,7 @@ http.createServer(function (req, res) {
    //id = urlObj.query.id
    res.write("<h2>This is my hello application</h2>");
    res.write ("Success!  This app is deployed online");
+     res.write ("Success!  This app is deployed online222");
   // res.write ("The id is: " + id)
    res.end();
   console.log('hey')
